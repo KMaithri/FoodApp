9ee4485d2b7854e9d2b8efcb3c308bf93b409c0c
Maithri Karasodi <maithri.karasodi@gmail.com>
2025-05-09 20:52:02 +0530

    added server.js to netlify functions

--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -1,6 +1,7 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
+// const fetch = require('node-fetch')
 
-export async function handler(event, context) {
+exports.handler= async function(event, context) {
   const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING", {
     headers: {
       'User-Agent': 'Mozilla/5.0',
--- netlify/functions/restaurant.js
+++ netlify/functions/restaurant.js
@@ -1,6 +1,7 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
+// const fetch = require('node-fetch')
 
-export async function handler(event, context) {
+exports.handler = async function(event, context) {
   const id = event.queryStringParameters.id;
 
   const data = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId=${id}`, {
